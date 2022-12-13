bebeaea7c6c9e5329ca24378d10f67f94abb5065
刘洋 <liuy.cosmo@haier.com>
2022-12-13 16:39:08 +0800

    方法类型优化

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
-import jwt_decode from 'jwt-decode'
+import jwt_decode, { JwtDecodeOptions } from 'jwt-decode'
 
 // 防抖
-export const debounce = (func: () => void, time = 500) => {
+export const debounce = (func: () => void, time = 500): void => {
   let timeId = window.timeId || null
   if (timeId) {
     clearTimeout(timeId)
@@ -12,18 +12,18 @@ export const debounce = (func: () => void, time = 500) => {
 }
 
 // 小数加%，保留两位小数
-export const toPer = (num: number) => {
+export const toPer = (num: number): string => {
   if (!num) return 0 + '%'
   return (num * 100).toFixed(2) + '%'
 }
 
 // 范围随机数
-export const genRandom = (minNum = 10, maxNum = 100) => {
+export const genRandom = (minNum = 10, maxNum = 100): number => {
   const strNum: string = Math.random() * (maxNum - minNum + 1) + minNum + ''
   return parseInt(strNum, 10)
 }
 
 // jwt token 解析
-export const jwtDecode = (token: string): {} => {
-  return jwt_decode(token);
+export const jwtDecode = (token: string, options?: JwtDecodeOptions | undefined): {} => {
+  return jwt_decode(token, options);
 }
