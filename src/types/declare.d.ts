3c4cb782832ecfa13559d0809cdccca88e924530
刘洋 <liuy.cosmo@haier.com>
2022-12-13 16:36:04 +0800

    update：类型优化

--- src/types/declare.d.ts
+++
@@ -1,4 +0,0 @@
-interface Window {
-  timeId: NodeJS.Timeout
-  [propName: string]: any
-}
--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,11 +1,15 @@
-/** 
+/**
  * 该文件仅存放全局类型的声明
  * 文件中不会出现 import、export 关键字
-*/
+ */
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+interface Window {
+  timeId: NodeJS.Timeout
 }
