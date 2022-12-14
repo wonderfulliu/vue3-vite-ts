e8565f05492755caf2bbdf4b93e655ecd56f9f59
刘洋 <liuy.cosmo@haier.com>
2022-12-14 09:26:17 +0800

    update: 调整样式引入顺序

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,14 @@ import 'element-plus/dist/index.css'
 
 // 样式格式化
 import 'normalize.css/normalize.css'
+// 引入全局样式
+import '@/styles/common.scss'
 
 // 引入 vue-router
 import router from '@/router'
 import '@/router/permission'
 // 引入 store
 import pinia from '@/store'
-
-// 引入全局样式
-import '@/styles/common.scss'
 
 const app = createApp(App)
 app
